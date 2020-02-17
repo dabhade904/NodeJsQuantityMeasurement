9a7c5b8be51f5056f771839441d7ca40091a61cb
dabhade904 <dabhade904@gmail.com>
2020-02-17 15:26:56 +0530

    Added : test case to check 1 yard are equal to 3 feet

--- Test/QuantityMeasurementTest.js
+++ Test/QuantityMeasurementTest.js
@@ -70,30 +70,30 @@ describe('Legth test cases', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 0);
         let length2 = new quantityMeasurement(Unit.INCH, 0);
         let ans = length1.compare(length2);
-        assert.equal(ans,true);
+        assert.equal(ans, true);
     });
 
     it('should return equal when 0 feet and 0 inch.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 0);
         let length2 = new quantityMeasurement(Unit.INCH, 0);
         let ans = length1.compare(length2);
-        assert.equal(ans,true);
+        assert.equal(ans, true);
     });
 
     it('should return notEqual when 1 feet and 1 inch.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 1);
         let length2 = new quantityMeasurement(Unit.INCH, 1);
         let ans = length1.compare(length2);
-        assert.notEqual(ans,true);
+        assert.notEqual(ans, true);
     });
 
     it('should return notEqual when 1 feet and 1 inch.', function () {
         let length1 = new quantityMeasurement(Unit.INCH, 1);
         let length2 = new quantityMeasurement(Unit.FEET, 1);
         let ans = length1.compare(length2);
-        assert.notEqual(ans,true);
+        assert.notEqual(ans, true);
     });
-    
+
     it('should return equal when 12 inch and 1 feet.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 1);
         let length2 = new quantityMeasurement(Unit.INCH, 12);
@@ -105,35 +105,41 @@ describe('Legth test cases', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 3);
         let length2 = new quantityMeasurement(Unit.YARD, 1);
         let ans = length1.compare(length2);
-        assert.equal(ans,true);
+        assert.equal(ans, true);
     });
 
     it('should return notEqual when 1 feet and 1 yard.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 1);
         let length2 = new quantityMeasurement(Unit.YARD, 1);
         let ans = length1.compare(length2);
-        assert.notEqual(ans,true);
+        assert.notEqual(ans, true);
     });
 
     it('should return notEqual when 1 inch and 1 yard.', function () {
         let length1 = new quantityMeasurement(Unit.INCH, 1);
         let length2 = new quantityMeasurement(Unit.YARD, 1);
         let ans = length1.compare(length2);
-        assert.notEqual(ans,true);
+        assert.notEqual(ans, true);
     });
 
-    it('should return notEqual when 1 yard and 36 inch.', function () {
+    it('should return equal when 1 yard and 36 inch.', function () {
         let length1 = new quantityMeasurement(Unit.YARD, 1);
         let length2 = new quantityMeasurement(Unit.INCH, 36);
         let ans = length1.compare(length2);
-        assert.equal(ans,true);
+        assert.equal(ans, true);
     });
 
-    it('should return notEqual when 36 inch and 1 yard.', function () {
+    it('should return equal when 36 inch and 1 yard.', function () {
         let length1 = new quantityMeasurement(Unit.INCH, 36);
         let length2 = new quantityMeasurement(Unit.YARD, 1);
         let ans = length1.compare(length2);
-        assert.equal(ans,true);
+        assert.equal(ans, true);
     });
 
+    it('should return equal when 1 yard and 3 feet', function () {
+        let length1 = new quantityMeasurement(Unit.YARD, 1);
+        let length2 = new quantityMeasurement(Unit.FEET, 3);
+        let ans = length1.compare(length2);
+        assert.equal(ans, true);
+    });
 })
