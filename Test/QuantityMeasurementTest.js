be3047999c90ad58c28087db5f433dbc6eaed770
dabhade904 <dabhade904@gmail.com>
2020-02-17 14:34:13 +0530

    Added :test case for checking null value

--- Test/QuantityMeasurementTest.js
+++ Test/QuantityMeasurementTest.js
@@ -1,5 +1,5 @@
 
-var assert = require('assert');
+var assert = require('chai').assert;
 var quantityMeasurement = require('../Main/QuantityMeasurement');
 var Unit = require('../Main/UnitType');
 
@@ -24,26 +24,33 @@ describe('Legth test cases', function () {
         let length2 = new quantityMeasurement(Unit.INCH, 12);
         let ans = length1.compare(length2);
         assert.equal(ans, true);
-      });
+    });
 
-      it('should return not equal when 1 feet and 0 feet.', function () {
+    it('should return not equal when 1 feet and 0 feet.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 1);
         let length2 = new quantityMeasurement(Unit.FEET, 0);
         let ans = length1.compare(length2);
         assert.equal(ans, false);
-      });
-      it('should return not equal when 1 inch and 0 inch.', function () {
+    });
+    it('should return not equal when 1 inch and 0 inch.', function () {
         let length1 = new quantityMeasurement(Unit.INCH, 1);
         let length2 = new quantityMeasurement(Unit.INCH, 0);
         let ans = length1.compare(length2);
         assert.equal(ans, false);
-      });
+    });
 
-      it('should return equal when 0 feet and 0 feet.', function () {
+    it('should return equal when 0 feet and 0 feet.', function () {
         let length1 = new quantityMeasurement(Unit.FEET, 0);
         let length2 = new quantityMeasurement(Unit.FEET, 0);
         let ans = length1.compare(length2);
         assert.equal(ans, true);
     });
-      
+
+    it('check for null.', function () {
+        let length = new quantityMeasurement(Unit.FEET, 0);
+        assert.isNotNull(length);
+    });
+
+   
+
 })
