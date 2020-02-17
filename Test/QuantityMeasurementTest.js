cea7f2832e24970def50dbcc990f0c6e0e51424e
dabhade904 <dabhade904@gmail.com>
2020-02-17 15:09:24 +0530

    Added : test case to check 1 feet notEqual to 1 inch

--- Test/QuantityMeasurementTest.js
+++ Test/QuantityMeasurementTest.js
@@ -72,4 +72,20 @@ describe('Legth test cases', function () {
         let ans = length1.compare(length2);
         assert.equal(ans,true);
     });
+
+    it('should return equal when 0 feet and 0 inch.', function () {
+        let length1 = new quantityMeasurement(Unit.FEET, 0);
+        let length2 = new quantityMeasurement(Unit.INCH, 0);
+        let ans = length1.compare(length2);
+        assert.equal(ans,true);
+    });
+
+    it('should return notEqual when 1 feet and 1 inch.', function () {
+        let length1 = new quantityMeasurement(Unit.FEET, 1);
+        let length2 = new quantityMeasurement(Unit.INCH, 1);
+        let ans = length1.compare(length2);
+        assert.notEqual(ans,true);
+    });
+
+    
 })
