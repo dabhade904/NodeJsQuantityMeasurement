a657773541fbe67c98ee2c456c668948103aef40
dabhade904 <dabhade904@gmail.com>
2020-02-17 14:13:50 +0530

    Added : test case to check 0 feet and 0 feet are equal

--- Main/QuantityMeasurement.js
+++ Main/QuantityMeasurement.js
@@ -2,22 +2,23 @@
 var unit = require('../Main/UnitType');
 
 class Measurement {
-	constructor(unit, value) {
-	    this.value = value; //for inch
-	    this.unit = unit;   //for feet 
-     
-	}
 
-	compare(that) {
-		if (this.unit == that.unit) {
-			return this.value == that.value;
-		} else if (this.unit != that.unit) {
-			return this.unit * this.value == that.unit * that.value;
-		} else {
-			return false;
-		}
-	}
+    constructor(unit, value) {
+        this.value = value; //for inch
+        this.unit = unit;   //for feet 
+
+    }
+
+    compare(units) {
+        if (this.unit == units.unit) {
+            return this.value == units.value;
+        } else if (this.unit != units.unit) {
+            return this.unit * this.value == units.unit * units.value;
+        } else {
+            return false;
+        }
+    }
 }
-module.exports =Measurement;
+module.exports = Measurement;
 
 
--- Test/QuantityMeasurementTest.js
+++ Test/QuantityMeasurementTest.js
@@ -20,11 +20,30 @@ describe('Legth test cases', function () {
     });
 
     it('should return equal when 1 feet and 12 inch.', function () {
-        let length1 = new quantityMeasurement(Unit.length.FEET, 1);
-        let length2 = new quantityMeasurement(Unit.length.INCH, 12);
+        let length1 = new quantityMeasurement(Unit.FEET, 1);
+        let length2 = new quantityMeasurement(Unit.INCH, 12);
+        let ans = length1.compare(length2);
+        assert.equal(ans, true);
+      });
+
+      it('should return not equal when 1 feet and 0 feet.', function () {
+        let length1 = new quantityMeasurement(Unit.FEET, 1);
+        let length2 = new quantityMeasurement(Unit.FEET, 0);
+        let ans = length1.compare(length2);
+        assert.equal(ans, false);
+      });
+      it('should return not equal when 1 inch and 0 inch.', function () {
+        let length1 = new quantityMeasurement(Unit.INCH, 1);
+        let length2 = new quantityMeasurement(Unit.INCH, 0);
+        let ans = length1.compare(length2);
+        assert.equal(ans, false);
+      });
+
+      it('should return equal when 0 feet and 0 feet.', function () {
+        let length1 = new quantityMeasurement(Unit.FEET, 0);
+        let length2 = new quantityMeasurement(Unit.FEET, 0);
         let ans = length1.compare(length2);
         assert.equal(ans, true);
     });
-
-    
+      
 })
